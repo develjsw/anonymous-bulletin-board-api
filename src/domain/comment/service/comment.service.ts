bdde9f2a091493a6809329d304edf1c0a5ccd525
jsw <develjsw@gmail.com>
2025-04-26 17:12:10 +0900

    [comment] 대댓글 등록 API 유효성 검증 추가 (댓글 Depth 제한)

--- src/domain/comment/service/comment.service.ts
+++ src/domain/comment/service/comment.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCommentCommand } from '../repository/command/create-comment.command';
 import { CreateCommentDto } from '../dto/create-comment.dto';
 import { GetCommentQuery } from '../repository/query/get-comment.query';
@@ -22,6 +22,12 @@ export class CommentService {
 
     async createCommentReply(parentId: number, dto: CreateCommentReplyDto): Promise<void> {
         const findCommentResult: CommentModel = await this.getCommentQuery.findCommentById(parentId);
+        const { parent_id } = findCommentResult;
+
+        // 댓글 무한 depth 방지 (추후 확장 원할때 주석처리)
+        if (parent_id) {
+            throw new BadRequestException('댓글은 대댓글까지만 가능합니다');
+        }
 
         if (!findCommentResult) {
             throw new NotFoundException('부모 댓글을 찾을 수 없습니다');
